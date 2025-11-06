9e30e43cafe22003806994b4433b0e199deaf8f3
DennisDong-1 <dong.dennis123@gmail.com>
2025-11-06 12:44:46 +0545

    Initial setup

--- main.py
+++ main.py
@@ -5,17 +5,3 @@ var = FastAPI()
 @var.get("/")
 def root():
     return {"question": "Show me who is on leave today."}
-
-# @var.get("/test")
-# def root():
-#     return {"Hello": "World"}
-
-# @var.post("/items")
-# def create_item(item: str):
-#     items.append(item)
-#     return items
-
-# @var.get("/items/{item_id}")
-# def get_item(item_id: int) -> str:
-#     item = items[item_id]
-#     return item
